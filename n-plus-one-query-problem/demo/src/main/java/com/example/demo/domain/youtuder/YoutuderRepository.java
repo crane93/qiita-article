32a66258951fcdca2796efbb0b7ae6882aaad4ab
crane93 <youl9094@gmail.com>
2022-09-24 00:32:01 -0400

    JPQLでn+1問題を解決方法

--- n-plus-one-query-problem/demo/src/main/java/com/example/demo/domain/youtuder/YoutuderRepository.java
+++ n-plus-one-query-problem/demo/src/main/java/com/example/demo/domain/youtuder/YoutuderRepository.java
@@ -1,6 +1,22 @@
 package com.example.demo.domain.youtuder;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface YoutuderRepository extends JpaRepository<Youtuder, Integer> {
+    /**
+        join fetchで参照
+     */
+    @Query("select o from Youtuder o join fetch o.videos")
+    List<Youtuder> findAllJoinFetch();
+
+    /**
+        EntityGraphで参照
+     */
+    @EntityGraph(attributePaths = "videos")
+    @Query("select a from Youtuder a")
+    List<Youtuder> findAllEntityGraph();
 }
